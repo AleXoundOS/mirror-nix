a922cb34ace728f1960830ab6f6ee14c3d8f6e7f
Alexander Tomokhov <alexoundos@gmail.com>
2019-05-05 01:59:25 +0300

    download status code and exception handling
    - The `StatusCodeException` type variable workaround makes me a bit
    stumped.

--- test/Test.hs
+++ test/Test.hs
@@ -4,25 +4,35 @@
 module Main where
 
 import Network.HTTP.Req
-import Network.HTTP.Req.Conduit
-import qualified Data.ByteString.Char8 as BS
-  (ByteString, putStrLn, writeFile)
-import Data.Default.Class
-import Control.Monad.IO.Class
+import Network.HTTP.Req.Conduit (responseBodySource)
+import Network.HTTP.Client (responseStatus, HttpExceptionContent(..))
+import Network.HTTP.Types.Status (ok200)
+import Data.Default.Class (def)
 import Conduit ((.|), runConduitRes, sinkFileCautious)
+import Control.Exception (try)
 
 
-testHostname :: Url 'Https
-testHostname = https "cache.nixos.org"
+defHost :: Url 'Https
+defHost = https "cache.nixos.org"
+-- defHost = https "httpbin.org"
 
 main :: IO ()
 main =
+  print =<<
   downloadFile "nar/1wf20f50458zf9x3dvrhyd04hzn7m71qg24aq1jsv01cvnb692a6.nar.xz"
-  -- BS.writeFile "testData.raw"
-  -- =<< downloadFile
-  -- "nar/1wf20f50458zf9x3dvrhyd04hzn7m71qg24aq1jsv01cvnb692a6.nar.xz"
+               "testFile.raw"
 
-downloadFile :: String -> IO ()
-downloadFile path = runReq def $ do
-  reqBr GET (testHostname /~ path) NoReqBody mempty $ \r ->
-    runConduitRes $ responseBodySource r .| sinkFileCautious "testFile.raw"
+downloadFile :: String -> FilePath -> IO (Maybe HttpException)
+downloadFile urlpath filepath = checkExcept <$> downloadAndSave
+  where
+    downloadAndSave = try $ runReq config $
+      reqBr GET (defHost /~ urlpath) NoReqBody mempty $ \r ->
+      runConduitRes $ responseBodySource r .| sinkFileCautious filepath
+    config = def {httpConfigCheckResponse = checkResponse}
+    checkResponse _ resp bs =
+      if responseStatus resp == ok200
+      then Nothing
+      else Just $ StatusCodeException (fmap (const ()) resp) bs -- :@
+    checkExcept :: Either HttpException () -> Maybe HttpException
+    checkExcept (Left e) = Just e
+    checkExcept _ = Nothing
