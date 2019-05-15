d1e22c6902e3e17893c8e3f0241c9d9b1f8650d5
Alexander Tomokhov <alexoundos@gmail.com>
2019-05-16 00:15:17 +0300

    store path parsing

--- src/System/Nix/Narinfo.hs
+++ src/System/Nix/Narinfo.hs
@@ -2,21 +2,27 @@
 
 module System.Nix.Narinfo where
 
-import Data.Text as T (Text, split, unpack)
+import qualified Data.Text as T
+import Data.Text (Text)
 import Data.YAML as Y
 
 
 data NarInfo = NarInfo
-  { _storePath   :: FilePath
+  { _storePath   :: Text
   , _url         :: !Text -- ^ nar file url compressed or uncompressed
   , _compression :: !NarCompressionType -- ^ compression type: bz2, xz, none
   , _fileHash    :: !Text -- ^ hash of nar file compressed or uncompressed
   , _fileSize    :: Int
   , _narHash     :: Text  -- ^ uncompressed nar file hash
   , _narSize     :: Int
-  , _references  :: !Text -- ^ other narinfo names this references (depends)
+  , _references  :: ![Text] -- ^ other store names this references (depends)
   , _deriver     :: Text
   , _sig         :: Text
+  } deriving Show
+
+data StorePath = StorePath
+  { _base32hash    :: !Text
+  , _pkgNameAndVer :: Text
   } deriving Show
 
 -- | Types of compression supported for NAR archives.
@@ -25,7 +31,7 @@ data NarCompressionType = CompBz2 | CompXz | CompNone
 
 instance FromYAML NarInfo where
   parseYAML (Mapping _ m) = NarInfo
-    <$> fmap T.unpack (m .: "StorePath")
+    <$> m .: "StorePath"
     <*> m .: "URL"
     <*> (parseNarCompression =<< m .: "Compression")
     <*> (parseFileHash =<< m .: "FileHash")
@@ -46,3 +52,30 @@ parseFileHash :: Monad m => Text -> m Text
 parseFileHash t = case T.split (== ':') t of
                     ["sha256", base32hash] -> pure base32hash
                     _ -> fail "sha256 `FileHash` cannot be parsed!"
+
+-- parseStoreNames :: Monad m => Text -> m [String]
+-- parseStoreNames t = if all True
+--                     then pure tWords
+--                     else fail $ "invalid store name in " ++ show tWords ++ "!"
+--   where
+--     tWords = T.words t
+
+mkStorePath :: Text -> Maybe StorePath
+mkStorePath t' =
+  let (_nixBasePath, p1) = T.splitAt 11 t'
+      (base32hash, p2) = T.splitAt 32 p1
+      (_hyphen, pkgNameAndVer) = T.splitAt 1 p2
+  in StorePath <$> mkBase32 base32hash <*> mkPkgNameAndVer pkgNameAndVer
+  where
+    mkBase32 :: T.Text -> Maybe T.Text
+    mkBase32 t = if all ($ t) [(== 32) . T.length , isBase32]
+                 then Just t
+                 else Nothing
+    mkPkgNameAndVer :: T.Text -> Maybe T.Text
+    mkPkgNameAndVer t = if all ($ t) [not . T.null, isAlphaNum]
+                        then Just t
+                        else Nothing
+    isBase32 = T.all (`elem` ("0123456789abcdfghijklmnpqrsvwxyz" :: String))
+    isAlphaNum =
+      T.all (`elem` ("+-.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                     \_abcdefghijklmnopqrstuvwxyz" :: String))
