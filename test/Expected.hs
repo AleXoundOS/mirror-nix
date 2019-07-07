2817b44ae88c8e002a794d484157cfc0823419f5
Alexander Tomokhov <alexoundos@gmail.com>
2019-07-07 22:39:02 +0300

    fix NarInfo parsing tests

--- test/Expected.hs
+++ test/Expected.hs
@@ -4,8 +4,8 @@ module Expected where
 
 import System.Nix.NarInfo
 
-correctNarInfoYAML :: NarInfo
-correctNarInfoYAML =
+rightNarInfoYaml1 :: Either String NarInfo
+rightNarInfoYaml1 = Right
   NarInfo
   {_storeHash = "003jhgnff2dwnz4j23wsqhzx9mdbxrqx"
   , _url = "nar/1an2kzz2gcwwchf6d8y2z70j1sy4r3m7wjlq10ia90dbys9ps1f3.nar.xz"
@@ -14,8 +14,7 @@ correctNarInfoYAML =
   , _fileSize = 32541236
   , _narHash = "sha256:1rq4cpf6adc9lqa5l4qnawpicd0ww36grzxl60a6wj3rjn0jm7d0"
   , _narSize = 125828016
-  , _references = ["003jhgnff2dwnz4j23wsqhzx9mdbxrqx"
-                  ,"37a8nkijf2vy350q3lxhjmxwdnpm6lnf"
+  , _references = ["37a8nkijf2vy350q3lxhjmxwdnpm6lnf"
                   ,"681354n3k44r8z90m35hm8945vsp95h1"
                   ,"7mrq5v4nnqfkv1ka7a0kdw9mzvqs5w3c"
                   ,"9429azblcx1y9apas3azxxd38imcmsy0"
@@ -23,7 +22,22 @@ correctNarInfoYAML =
                   ,"l3fbpz66yfjvamy0bi9fjlhwpp57g14y"
                   ,"sr4253np2gz2bpha4gn8gqlmiw604155"
                   ,"zx64bk34mqmgfrkbx6ls4hzzxzckah49"]
-  , _deriver = "hvgi75bvyzj2zn73qyxxxi788lh3kncr-gcc-7.4.0.drv"
+  , _deriver = Just "hvgi75bvyzj2zn73qyxxxi788lh3kncr-gcc-7.4.0.drv"
   , _sig = "cache.nixos.org-1:\
            \fitLERVREZX0cozJ4HlJxwB2dVRTT6ZipTyAoAzEpcDaBiV3J2xU+wEKHkspm9hR\
            \ISgAlJ/iOzjFy781/b6eCw=="}
+
+rightNarInfoYaml2 :: Either String NarInfo
+rightNarInfoYaml2 = (\x -> x {_references = []}) <$> rightNarInfoYaml1
+
+rightNarInfoYaml3 :: Either String NarInfo
+rightNarInfoYaml3 = (\x -> x {_deriver = Nothing}) <$> rightNarInfoYaml2
+
+leftNarInfo1 :: Either String NarInfo
+leftNarInfo1 = Left "Aeson exception:\nError in $: NarInfo YAML parsing Error! Invalid reference in:\n003jhgnff2dwnz4j23wsqhzx9mdbxrqx-gcc-7.4.0 37a8nkijf2vy350q3lxhjmxwdnpm6lnf-isl-0.17.1 681354n3k44r8z90m35hm8945vsp95h1-glibc-2.27 7mrq5v4nnqfkv1ka7a0kdw9mzvqs5w3c-gmp-6.1.2 9429azblcx1y9apas3azxxd38imcmsy0-mpfr-4.0.2 iiymx8j7nlar3gc2lfkcscvr61fng8s-zlib-1.2.11 l3fbpz66yfjvamy0bi9fjlhwpp57g14y-gcc-7.4.0-lib sr4253np2gz2bpha4gn8gqlmiw604155-glibc-2.27-dev zx64bk34mqmgfrkbx6ls4hzzxzckah49-libmpc-1.1.0"
+
+leftNarInfo2 :: Either String NarInfo
+leftNarInfo2 = Left "Aeson exception:\nError in $: NarInfo YAML parsing Error! `Compression` type read from Narinfo is not `xz`:\nnone"
+
+leftNarInfo3 :: Either String NarInfo
+leftNarInfo3 = Left "Aeson exception:\nError in $: NarInfo YAML parsing Error! Invalid store path:\n/nix/storage/003jhgnff2dwnz4j23wsqhzx9mdbxrqx-gcc-7.4.0"
--- test/Test.hs
+++ test/Test.hs
@@ -5,12 +5,9 @@ module Main where
 
 import Test.Tasty
 import Test.Tasty.HUnit
-import Network.HTTP.Req
-import qualified Data.ByteString.Lazy as B
-import qualified Data.YAML as Y
-import Data.Either (isLeft)
+import qualified Data.Yaml as Y
+  (decodeFileEither, decodeEither', prettyPrintParseException)
 
-import Download
 import System.Nix.NarInfo
 
 import qualified Expected as E
@@ -21,33 +18,30 @@ main = defaultMain narInfoParsing
 
 narInfoParsing :: TestTree
 narInfoParsing = testCase "NarInfo parsing" $ do
-  bs <- B.readFile "test-data/003jhgnff2dwnz4j23wsqhzx9mdbxrqx.narinfo"
-  assertEqual "parse correct NarInfo YAML"
-    (Right E.correctNarInfoYAML) (Y.decode1 bs)
-  caseParseNarInfo
+  isEqualParseNarInfo "parse correct NarInfo YAML"
+    "test-data/003jhgnff2dwnz4j23wsqhzx9mdbxrqx.narinfo"
+    E.rightNarInfoYaml1
+  isEqualParseNarInfo "parse correct NarInfo YAML with empty References"
+    "test-data/003jhgnff2dwnz4j23wsqhzx9mdbxrqx-empty-references.narinfo"
+    E.rightNarInfoYaml2
+  isEqualParseNarInfo "parse correct NarInfo YAML with empty References"
+    "test-data/003jhgnff2dwnz4j23wsqhzx9mdbxrqx-no-deriver.narinfo"
+    E.rightNarInfoYaml3
+  isEqualParseNarInfo "parse of bad NarInfo YAML 1"
     "test-data/003jhgnff2dwnz4j23wsqhzx9mdbxrqx-bad1.narinfo"
-    "false parse of bad NarInfo YAML"
-    isLeft
-    (Y.decode1 :: B.ByteString -> Either String NarInfo)
-  caseParseNarInfo
+    E.leftNarInfo1
+  isEqualParseNarInfo "parse of bad NarInfo YAML 2"
     "test-data/003jhgnff2dwnz4j23wsqhzx9mdbxrqx-bad2.narinfo"
-    "false parse of bad NarInfo YAML"
-    isLeft
-    (Y.decode1 :: B.ByteString -> Either String NarInfo)
-  caseParseNarInfo
+    E.leftNarInfo2
+  isEqualParseNarInfo "parse of bad NarInfo YAML 3"
     "test-data/003jhgnff2dwnz4j23wsqhzx9mdbxrqx-bad3.narinfo"
-    "false parse of bad NarInfo YAML"
-    isLeft
-    (Y.decode1 :: B.ByteString -> Either String NarInfo)
-  caseParseNarInfo
-    "test-data/003jhgnff2dwnz4j23wsqhzx9mdbxrqx-bad4.narinfo"
-    "false parse of bad NarInfo YAML"
-    isLeft
-    (Y.decode1 :: B.ByteString -> Either String NarInfo)
+    E.leftNarInfo3
 
 -- | Make a test case which reads a file, applies a function to it and asserts
 -- the result with the given condition.
-caseParseNarInfo :: FilePath -> String -> (b -> Bool) -> (B.ByteString -> b)
-                 -> Assertion
-caseParseNarInfo filepath msg cond func =
-  (cond . func <$> B.readFile filepath) @? msg
+isEqualParseNarInfo
+  :: HasCallStack => String -> FilePath -> Either String NarInfo -> Assertion
+isEqualParseNarInfo msg filepath expected =
+  assertEqual msg expected
+  =<< (either (Left . Y.prettyPrintParseException) Right
+        <$> Y.decodeFileEither filepath)
