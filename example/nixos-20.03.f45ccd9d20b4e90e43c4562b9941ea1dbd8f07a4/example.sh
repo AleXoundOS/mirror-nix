d9cef16645460541a87816cdf052745126387d01
Alexander Tomokhov <alexoundos@gmail.com>
2020-05-23 04:01:17 +0300

    add example

---
+++ example/nixos-20.03.f45ccd9d20b4e90e43c4562b9941ea1dbd8f07a4/example.sh
@@ -0,0 +1,10 @@
+NIXPKGS="https://github.com/NixOS/nixpkgs/archive/f45ccd9d20b4e90e43c4562b9941ea1dbd8f07a4.tar.gz"
+stack exec -- nix-mirror-cache `
+`--nixpkgs="$NIXPKGS" `
+`--dump-paths="paths.dump" `
+`--dump-narinfo-urls="narinfo-urls.dump" --dump-nar-urls="nar-urls.dump" `
+`--store-paths="store-paths" `
+`--release-combined-json="instantiate-release-combined-recursive.json" `
+`--ofborg-outpaths-out="nix-env-ofborg-outpaths.txt" `
+`--find-fixed-outputs-json="find-fixed-outputs-pp.json" `
+` $@
